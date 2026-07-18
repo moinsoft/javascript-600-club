4f1a747ba3bfae725359148db5817aa2845941ef
moinsoft-dev <moinsoft.dev@gmail.com>
2026-07-19 02:48:46 +0600

    feat: add number variable solutions

---
+++ solutions/01-variables/variables.js
@@ -0,0 +1,61 @@
+/**
+ * JavaScript 600 Club
+ * Chapter: Variables
+ *
+ */
+
+
+
+// ===============================
+// Problem 01-14: Number Variables
+// ===============================
+
+// Problem 01: Store birth year in a variable
+const birthYear = 2000;
+
+// Problem 02: Store number of siblings
+const siblings = 7;
+
+// Problem 03: Store number of students in class
+const students = 45;
+
+// Problem 04: Store exam marks
+const marks = 900;
+
+// Problem 05: Store number of rooms in home
+const rooms = 6;
+
+// Problem 06: Store daily study hours
+const dailyStudyHours = 7;
+
+// Problem 07: Store sum of phone number digits
+const total = 18;
+
+// Problem 08: Store current age
+const age = 26;
+console.log(age);
+
+// Problem 09: Store planned books to read this year
+const booksToRead = 12;
+console.log(booksToRead);
+
+// Problem 10: Store wake up time and update value
+var wakeUpTime = 7;
+wakeUpTime = 6;
+console.log(wakeUpTime);
+
+// Problem 11: Store wasted mobile phone time
+const wastedTime = 4;
+console.log(wastedTime);
+
+// Problem 12: Store borrowed money
+const borrow = 500;
+console.log(borrow);
+
+// Problem 13: Store maximum goals scored by favorite team
+const goals = 5;
+console.log(goals);
+
+// Problem 14: Store number of pillows
+const pillow = 2;
+console.log(pillow);
